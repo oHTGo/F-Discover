6de90b82f0dbe4476a9b6a973e0d89e316cd1aa2
HTG <nguyennhathuy.orit@gmail.com>
2021-06-04 15:44:33 +0700

    Add feature get post by id

---
+++ post/interfaces/info.go
@@ -0,0 +1,16 @@
+package IPost
+
+type Author struct {
+	ID        string `json:"id"`
+	Name      string `json:"name"`
+	AvatarUrl string `json:"avatar_url"`
+}
+
+type Info struct {
+	ID      string   `json:"id"`
+	Content string   `json:"content"`
+	Images  []string `json:"images"`
+	Videos  []string `json:"videos"`
+	Likes   int      `json:"likes"`
+	Author  Author   `json:"author"`
+}
