4790a6c198f6eae2dd3bb84553c36aa610d9925e
HTG <nguyennhathuy.orit@gmail.com>
2021-06-08 11:19:47 +0700

    Add helper deleteFile

---
+++ helpers/deleteFile.go
@@ -0,0 +1,21 @@
+package helpers
+
+import (
+	"f-discover/env"
+	"f-discover/instance"
+	"f-discover/services"
+	"strings"
+)
+
+func DeleteFile(path string) {
+	var object string
+	if ok := strings.Contains(path, env.Get().STORAGE_BUCKET); ok {
+		runes := []rune(path)
+		position := strings.Index(path, env.Get().STORAGE_BUCKET) + len(env.Get().STORAGE_BUCKET) + 1
+		object = string(runes[position:])
+	} else {
+		object = path
+	}
+	bucket := services.GetInstance().StorageBucket
+	bucket.Object(object).Delete(instance.CtxBackground)
+}
