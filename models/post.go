142544331dd3aa33bb2fe489d08de38f42f82987
HTG <nguyennhathuy.orit@gmail.com>
2021-06-08 16:26:58 +0700

    Update post model

--- models/post.go
+++ models/post.go
@@ -7,6 +7,6 @@ type Post struct {
 	Content string                 `firestore:"content" json:"content"`
 	Images  []string               `firestore:"images" json:"images"`
 	Videos  []string               `firestore:"videos" json:"videos"`
-	Likes   []map[string]bool      `firestore:"likes" json:"likes"`
+	Likes   map[string]bool        `firestore:"likes" json:"likes"`
 	Author  *firestore.DocumentRef `firestore:"author" json:"author"`
 }
